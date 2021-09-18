e8cfb6c0919de45c4d399bc73ad538b38f21528f
Iqbal Fauzi <iqbal.fauzi.if99@gmail.com>
2021-09-18 07:02:48 +0700

    Build version 3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ object Apps {
     const val buildTools = "30.0.3"
     const val minSdk = 21
     const val targetSdk = 30
-    const val versionCode = 2
+    const val versionCode = 3
     const val versionName = "1.0.0"
 }
 
