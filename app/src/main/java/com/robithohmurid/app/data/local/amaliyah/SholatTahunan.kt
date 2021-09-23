80e66a850b4bb3c0f25a32b69d2786c50770fbd0
Iqbal Fauzi <iqbal.fauzi.if99@gmail.com>
2021-09-23 15:57:51 +0700

    Sholat Tahunan

--- app/src/main/java/com/robithohmurid/app/data/local/amaliyah/SholatTahunan.kt
+++ app/src/main/java/com/robithohmurid/app/data/local/amaliyah/SholatTahunan.kt
@@ -8,15 +8,160 @@ import com.robithohmurid.app.data.model.entity.ListingEntity
  */
 val listSholatTahunan = listOf(
     ListingEntity(0,
-    "Sholat Rojab", """
-        
+    "Sholat Rojab", """  
+        Jumlah keseluruhan shalat sunat Rojab adalah 42 rakaat yang dilaksanakan di antara waktu maghrib & Isya’ yaitu:  
+        10 rakaat pada tanggal 1 Rojab  
+        12 rakaat pada malam Jumaat pertama  
+        10 rakaat berikutnya pada tanggal 15 Rojab  
+        10 rakaat berikutnya pada akhir Rojab  
+
+        Niat shalat Rojab:  
+        ### أُصَلِّي سُنَّةً لِشَهْرِ رَجَبَ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ لِلهِ تَعَالَى
+        *ushollii sunnatan lisyahri rojab rok'ataini mustakbilal qiblata lilaahi ta'aala*
+
+        **Tanggal 1 Rajab:**  
+        Jumlah 10 rokaat (5 kali salam)  
+        Surat yang dibaca pada setiap roka’at setelah Al-Fatihah, Al-Ikhlas 3x, Al Kafirun 3x  
+        Doa yang dibaca setelah melaksanakan shalat Rojab pada tanggal 1 adalah:  
+        ### لآاِلهَ اِلاَّ اللهُ وَحْدَهُ لاَشَرِيْكَ لَهُ. لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ يُحْيِيْ وَيُمِيْتُ وَهُوَ حَيٌّ لاَيَمُوْتُ بِيَدِهِ الْخَيْرُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٍ. اللهم لاَمَانِعَ لِمَا أَعْطَيْتَ وَلاَ مُعْطِيَ لِمَا مَنَعْتَ وَلاَيَنْفَعُ ذَالْجَدِّ مِنْكَ الْجَدِّ.
+
+        *Laa ilaaha illalloohu wahdahu laa syariika lah, lahul mulku wa lahul hamdu yuhyii wa yumiitu wa huwa hayyul laa yamuutu biyadihil khoiru wa huwa 'alaa kulli syai-'in qodiir, alloohumma laa maani'a limaa 'athoita wa laa mu'thiya limaa mana'ta wa laa yanfa'u dzal jaddi minkal jaddi.*
+
+        **Sholat sunnat malam Jum’at pertama bulan Rajab:**  
+        Surat yang dibaca pada setiap roka’at setelah Al-Fatihah, Al-Qodar 3x, Al-Ikhlas 12x.
+        Dan setelah selesai sholat 12 roka’at (6 kali salam) kemudian membaca sholawat 70 kali dalam keadaan duduk seperti tasyahud awal, inilah sholawatnya:  
+        ### اللهم صَلِّ عَلَى مُحَمَّدِ النَّبِيِّ اْلأُمِّيِّ وَعَلَى ألِهِ وَسَلِّمْ
+        *Allohumma sholli ‘ala sayyidina Muhammadin Nabiyyil Ummiyyi wa ‘ala alihi wa shohbihi wa sallim*  
+
+        Setelah sholat kemudian bersujud sambil membaca Tasbih 70 kali dengan bacaan Tasbih sebagai berikut:  
+        ### سُبُّوْحٌ قُدُّوْسٌ رَبُّ الْمَلآئِكَةِ وَالرُّوْحِ
+        *Subbuhun quddusun robbul malaikati war ruh*  
+
+        Kemudian bandung dari sujud, duduk seperti semula dengan membaca Istighfar 70 kali, dengan Istiqfar sebagai berikut:  
+        ### رَبِّ اغْفِرْ وَارْحَمْ وَتَجَاوَزْ عَمَّا تَعْلَمْ فَاءِنَّكَ أَنْتَ الْعَزِيْزِالأَعْظَمْ
+        *Robbighfir warham wa tajawaz ‘amma ta’lamu fa innaka antal azizul a’zhom*  
+
+        Kemudian sujud kembali dan membaca Tasbih 70 kali seperti di atas ketika sujud **mohonkan ampunan kepada Alloh dan mohon segala apa yang dikehendaki untuk kebaikan dunia dan akhirat.**  
+        **Catatan**:  Sholat ini diawali dengan puasa pada hari kamisnya.  
+
+        **Sholat sunnat pada tanggal 15 Rojab:**  
+        Jumlah 10 rokaat (5 kali salam)  
+        Surat yang dibaca pada setiap roka’at setelah Al-Fatihah, Al-Ikhlas 3x, Al-Kafirun 3x  
+        Doa yang dibaca setelah melaksanakan shalat rajab pada tanggal 15 adalah:  
+        ### لآاِلهَ اِلاَّ اللهُ وَحْدَهُ لاَشَرِيْكَ لَهُ. لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ يُحْيِيْ وَيُمِيْتُ وَهُوَ حَيٌّ لاَيَمُوْتُ بِيَدِهِ الْخَيْرُ
+        ### وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٍ. اِلهًا واَحِدًا صَمَدًا فَرْدًا وِتْرًا لَمْ يَتَّخِذْصَاحِبَةً وَلاَوَلَدًا
+
+        *Laa ilaaha illalloohu wahdahu laa syariika lah, lahul mulku wa lahul hamdu yuhyii wa yumiitu wa huwa hayyul laa yamuutu biyadihil khoiru wa huwa ‘alaa kulli syai-ing qodiir, illahaw waahidan ahadan shomadan fardan witron lam yattakhidz shoohibatan wa laa waladan.*
+
+        **Sholat sunnat pada tanggal 30 Rojab:**  
+        Jumlah 10 rokaat (5 kali salam)  
+        Surat yang dibaca pada setiap roka’at setelah Al-Fatihah, Al-Ikhlas 3x, Al-Kafirun 3x  
+        Doa yang dibaca setelah melaksanakan shalat rajab pada tanggal 30 adalah:  
+        ### لآاِلهَ اِلاَّ اللهُ وَحْدَهُ لاَشَرِيْكَ لَهُ. لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ يُحْيِيْ وَيُمِيْتُ وَهُوَ حَيٌّ لاَيَمُوْتُ بِيَدِهِ الْخَيْرُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٍ. وَصَلّى اللهُ عَلَى سَيِّدِنَا مُحَمَّدٍ وَعَلى ألِهِ الطَّاهِرِيْنَ وَلاَحَوْلَ وَلاَقُوَّةَ اِلاَّبِاللهِ الْعَلِيِّ الْعَظِيْمِ.
+
+        *Laa ilaaha illalahu wahdahu la syariikalah Lahul mulku walahulhamdu yuhyi wayumiitu wahuwa hayyun. Laa yamuutu biyadihil khoiru wahuwa 'ala kulli syaiin qodiir. Wa shollallohu 'ala sayyidina muhammadin wa 'alaa alihitthohiriina wa lahaula wa la quwwata illa billahil 'aliyyil adzhim*  
+
+        **Wirid yang dibaca setiap setelah Maghrib dibulan rajab.**  
+        **Dari tanggal 1 sampai 10 membaca :**  
+        ### سُبْحَانَ اللهِ الْحَيُّ الْقَيُّوْمِ
+        *Subhaanalloohil hayyul qoyyuum 100x*  
+        **Dari tanggal 11 sampai 20 membaca:**  
+        ### سُبْحَانَ اللهِ اْلأَحَدِالصَّمَدِ
+        *Subhaanalloohil ahadish shomad 100x*  
+        **Dari tanggal 21 sampai tanggal 30 membaca :**  
+        ### سُبْحَانَ اللهِ الرَّؤُفِ
+        *Subhaanalloohir rouuf 100x*  
+
+        **Do’a Rojab**  
+        ### اللهم طهر لسانى من الكذب، وقلبى من النفاق، وعملى من الرياء، وبصرى من الخيانة، فإنك تعلم خائنة الأعين وما تخفى الصدور
+        *Allohumma thohhir lisaanii minal kidzbi, wa qolbii minan nifaq, wa 'amalii minar riyaa'i, Wa bashorii minal khiyaanati, fainnaka ta'lamu khoo'inatal a'yun wa maa tukhfis shuduur.*
     """.trimIndent()),
     ListingEntity(1,
     "Sholat Nisfu Sa'ban", """
-        
+        **Waktu : 15 Sya’ban (setelah sholat Magrib)  
+        Jumlah Rakaat : 100 rakaat (50 kali salam)  
+        Niat Sholat Nisfu Sa’ban :**  
+        ### اُصَلِّى سُنَّةَ نِصْفِ شَعْبَانَ رَكْعَتَيْنِ لِّله تَعَا لَى
+        *"Usholi sunnata Nisfi Sya'ban rok' ataini lillahi ta'ala"*  
+        Bacaan : Setelah Al-Fatihah membaca Al-Ikhlas 10x tiap rakaat.  
+
+        Menurut Kitab **Al-Guniyah Litholibi Thariqil – Haq** yang dikarang oleh Tuan Syeikh Abdul Qodir jilid 1 halaman 192 adalah jumlahnya 100 rakaat (50 kali salam) dan setiap rakaat setelah AL-Fatihah membaca surat AL-Ikhlas (Qulhu) sebanyak 10 kali. Sholat Nisfu Sya'ban ini disebut juga Sholat Khoir, karena banyaknya keutamaan dan pahala dari Alloh SWT.  
+
+        Sebagaimana diriwayatkan oleh Al Hasan Rohimulloh bahwa 30 sahabat berkata kepada ku: "Barang siapa yang mengerjakan Sholat pada malam Nisfu Sya'ban, maka Alloh SWT akan melirik kepadanya sebanyak 70 lirikan dan setiap lirikan, Alloh SWT mengabulkan 70 kebutuhan. Dan pemberian yang terendah adalah ampunan Alloh SWT".
     """.trimIndent()),
-    ListingEntity(1,
-        "Sholat Sunat Lailatul Qodar", """
+    ListingEntity(2,
+        "Sholat Tarowih", """
+            
+            Sholat Tarowih 600 rokaat di cicil 20 rokaat tiap hari. Bacaan tiap rokaatnya sebagai berikut:  
+            Rokaat 1: At Takasur 1-4  
+            Rokaat 2: At Takasur 5-8  
+            Rokaat 3: Al Asr 1-3  
+            Rokaat 4: Yassiin 58-59  
+            Rokaat 5: Al Humazah 1-3  
+            Rokaat 6: Al Humazah 4-9  
+            Rokaat 7: Al Fil 1-3  
+            Rokaat 8: Al Fil 4-5  
+            Rokaat 9: Al Quraisy 1-2  
+            Rokaat 10: Al Quraisy 3-4  
+            Rokaat 11: Al Maun 1-3  
+            Rokaat 12: Al Maun 4-7  
+            Rokaat 13: Al Kautsar 1-3  
+            Rokaat 14: An Nasr 1-3  
+            Rokaat 15: Al Kafirun 1-6  
+            Rokaat 16: Al Ikhlas 1-4  
+            Rokaat 17: An Nasr 1-3  
+            Rokaat 18: Al Lahab 1-5  
+            Rokaat 19: Al Falaq 1-5  
+            Rokaat 20: An Nas 1-6  
+
+            Pada setiap 2 raka’at 1x salam membaca Shalawat :  
+            ### ّللّهُمَّ صَلِّى عَلى سَيِّدِنّا مُحَمَّد
+            **Alloohhumma Sholli ‘alaa Sayyidina Muhammad**
+            Ya Alloh, berikanlah selalu Rahmat-Mu kepada Junjunanku Rosululloh Muhammad Sholallohu ‘alaihi wasalam’  
+
+            Dan dijawab :  
+            ### ّللّهُمَّ صَلِّى وَسَلِّمْ وَبَرِكْ عَلَيْهْ
+            *Allohhuma Sholli wa Sallim wa Barik ‘Alaihh*  
+            Ya Alloh, limpahkan pula Rahmat-Mu kepada keluarganya  
+
+            Pada setiap 4 raka’at membaca do’a:
+            ### رَبَّنَا تَقَبَّلْ مِنَّا وُضُوْ اَنَا وَ صَلَاةَ نَا وَ صِيَامَنَا وَقِيَامَنَا وَرُقُوْعَنَا وَسُجُوْدَنَا وَخُشُوْعَنَا وَتَخَشُوْعَنَا وَلى تَظْرِبِّهَا وُجُوْحَنَا ياَ اِلىهَلْ عَلَمِيْن وَيَا خَيْرُنَّاسِرِيْن بِرَحْمَتِكَ يَا اَرْحَمَرَحِمِيْن
+
+            *Robbanaa Taqobbal Minna Wudhuu anaa wa Sholaatanaa wa Shiyaamanaa wa Qiyaamanaa wa Rukuu ‘anaa wa Sujuudanaa wa Khusyuu ‘anaa wa Takhosyuu ‘anaa wa Laa Tadhrib bihaa wujuuhana Yaa Ilahal ‘alamiin wa Yaa khoirunnasirrin birohmatika yaa arhamarrohimiin.*
+
+            **Do’a Kamilin.**  
+            *Allohummaj Alna Bil Imani Kamilin, Wa Lil fa ro idli mu'addin, walis Sholati Hafizhin, Waliz Zakati Fa'Ilin, Wa Lima 'Indaka Tholibin, Wa li'Afwika rojin, Wa Bil-Huda Mutamas Sikin, Wa'Anil Laghwi Mu'-Ridlin. Wa Fid Dunya Zahidin, Wa Fil Akhiroti Roghibin 'Wa Bil qodlo'i rodlin, Wa Bin-na'Ma'i Syakirin, Wa'Alal-Bala-I Shobirin Wa Tah ta Liwa'I Sayyi dina, Muhammadin sholallahu 'alaihi wa sallam, Yaumal'Qiyamati Sa'irin Wa Ilal Hawdli Wa ridin Wa Fil Jannati Dakhilin Wa Minan naari Naajiin Wa'Ala Sariril-karomati Qo'idin Wa Bihurin 'Inin Mutazaw Wijin, Wa Min Sundusin Wa Istabroqin Wa Dibajin Mutalab Bisin, Wa Min Tho A'amil Jan nati Akilin, Wa Min Labanin Wa'Asalin Mushof-fan Syaribin, Bi-Ak wabin Wa Abariqo Wa Ka'sin Mim Ma'in Ma'Al-Ladzina An'Amta 'Alaihim Minan-Nabiy-yin, Was-Shiddiqin, Was-Syuhada'I WasShalihin. Wa Hasuna Ula'Ika Rofiqo, Dzalikal Fadl Lu Minalllohi Wa Kafa Billahi 'Alima. Allahummaj'Alna Fi haa Dzihil Lailatis Syarifatil Mubarokati Minas-su'Ada-il- Maqbulina Wa la taj`Alna Minal-Asyqiya-Il 'Mardudin. Washollalloohu 'alaa sayyidinaa Muhammadin wa Aalihii wa shohbihii ajma'iin. Birohmatika ya Ar hamar-rohimin. Walhamdu lillaahi robbil'aalamiin.*  
+
+            Dengan nama Alloh, Yang Maha Pengasih lagi Maha Penyayang  
+
+            Ya Alloh, jadikanlah kami orang-orang yang sempurna imannya, yang melaksanakan kewajiban-kewajiban terhadap-Mu, yang memelihara sholat, yang mengeluarkan zakat, yang mencari apa yang ada di sisi-Mu, yang mengharapkan ampunan-Mu, yang berpegang pada petunjuk, yang berpaling dari kebatilan, yang zuhud di dunia, yang menyenangi akhirat , yang ridho dengan ketentuan, yang ber'syukur atas nikmat yang diberikan, yang sabar atas segala musibah, yang berada di bawah panji-panji junjungan kami, Nabi Muhammad Saw, pada hari kiamat, sampai kepada telaga (yakni telaga Nabi Muhammad) yang masuk ke dalam surga, yang duduk di atas dipan kemuliaan, yang menikah dengan para bidadari, yang mengenakan berbagai sutra, yang makan makanan surga, yang minum susu dan madu yang murni dengan gelas, cangkir, dan cawan bersama orang-orang yang Engkau beri nikmat dari para nabi, shiddiqin, syuhada dan orang-orang sholih. Mereka itulah teman yang terbaik. Itulah keutamaan (anugerah) dari Alloh, dan cukuplah bahwa Alloh Maha Mengetahui.  
+
+            Ya Allah, jadikanlah kami pada malam yang mulia dan diberkahi ini tergolong orang-orang yang bahagia dan diterima amalnya, dan janganlah Engkau jadikan kami tergolong orang-orang yang celaka dan ditolak amalnya,  
+
+            Semoga Alloh mencurahkan rahmat-Nya atas penghulu kita Muhammad Saw, keluarga beliau dan shahabat beliau semuanya, berkat rahmat-Mu, oh Tuhan, Yang Paling Penyayang di antara yang penyayang.  
+
+            Segala puji bagi Alloh Tuhan semesta alam.
+
+            Setelah selesai sholat, mengucapkan lafadz niat puasa untuk esok hari :  
+            ### نَوَيْتُ صَوْمَ غَدٍ عَنْ أَدَ آءِ فَرْضِ شَهْرِ رَمَضَانَ هٰذِهِ السَّنَةِلِلّٰهِ تَعَلٰى إِيْمَانًا وَاحْتِسَابًا لِوَجْهِهِ الْكَرِيْمِ
+            *Na Way tu Shouma Ghodin 'An Ada-I Far Dli Syahri Romadloona HaaDzihis Syanati Lillahi Ta'ala, Iimaanaw Wah Tisaa Ban, Liwaj Hihil Kariim.*  
+
+            **Catatan**: Khotaman Ba’da Magrib selama bulan Romadhon dipindahkan waktunya menjadi Ba’da Sholat Ashar.
+        """.trimIndent()),
+    ListingEntity(3,
+    "Sholat Sunat Lailatul Qodar", """
         
-    """.trimIndent()),
+        Sholat Lailatul Qodar dimulai pada malam ke 21 ramadhan sampai akhir Romadhon. Sholat lailatul qodar 4 rokaat 2 kali salam. Setelah Sholat sunnat Lidafil Bala.  
+        Niatnya :  
+        ### اُصَلِّى سُنَّةً لَيْلَةُ الْقَدْرِ رَكْعَتَيْنِ لِّلهِ تَعَا لى اَللهُ اَكْبَرُ
+        *Usholii sunnatan sukri laelatul qodar rok'aataeni lillahi ta'aalaa Allohu akbar.*  
+        Sengaja aku sholat sunah laelatul qodar dua rakaat krn Alloh ta'ala.  
+        Surat yang dibaca pada setiap roka’at setelah Al-Fatihah, Attakatsur 1x, Al Ikhlas 3x  
+
+        Sholat Sunat ini Dilaksanakan sesudah sholat tarawih  
+        Sesudah selesai kemudian membaca do’a sebagai berikut:  
+        ### اَللَّهُمَّ اِنَّكَ عَفُوٌّ كَرِيْمٌ تُحِبُّ الْعَفْوَ، فَاعْفُ عَنَّا
+        *Allohumma innaka afuwwun tuhibbul afwa fa'fuannaa 3x*  
+        Yaa Alloh sesungguhnya Engkau pemaaf, suka memaafkan maka maafkanlah kami.
+    """.trimIndent())
 )
