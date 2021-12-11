a6139631efb9f88db9a40280d8c7f1a5d29fe197
Priambudi Sutanto <65936846+priambudi19@users.noreply.github.com>
2021-12-11 13:49:46 +0700

    revisi tahap 1

--- app/src/main/java/com/robithohmurid/app/data/local/amaliyah/manaqib/manqobah/Muqodimah.kt
+++ app/src/main/java/com/robithohmurid/app/data/local/amaliyah/manaqib/manqobah/Muqodimah.kt
@@ -3,26 +3,67 @@ package com.robithohmurid.app.data.local.amaliyah.manaqib.manqobah
 import com.robithohmurid.app.data.model.entity.ListingEntity
 
 val muqodimahData = ListingEntity(0, "Muqodimah","""
-    **MUQODIMAH MANQOBAH**
+    **MANQOBAH**
 
-    *YAA IMAMAL MUTTAQIINA*  
-    (Artinya: Wahai pemimpin orang-orang yang bertaqwa)  
-    *WA SHOFWATAL 'AABIDIINA*  
-    (Artinya: Wahai ahli ibadah yang terbaik)  
-    *YAA QOWIYAL ARKAAN*  
-    (Artinya: Wahai yang kokoh pilarnya)  
-    *YAA HABIBAR ROHMAAN*  
-    (Artinya: Wahai kekasih Ar Rohman)  
-    *YAA MUJLIYAL KALAAMIL QODIMI*  
-    (Artinya: Wahai yang menyingkap Kalam Al Qodim)  
-    *YAA SYIFAAA ASQOOMIS SAQIIMI*  
-    (Artinya: Wahai penyembuh penyakit orang yang sakit)  
+    MUQODIMAH MANQOBAH
 
-    Bismillaahirrohmaanirroohim. Alaa inna Auliyaa'alloohi laa khoufun ‘alaihim wa laahum yahzanuun. Alladziina aamanuu wakaanuu yattaquun. Lahumul busyroo fil hayaatid dunyaa wa fiil aakhiroh, laa tabdiila likalimaatillaah, dzaalika huwal fauzul ‘azhiim  
+    _Assalamu'alaikum warohmatullohi wabarokatuh_
 
-    Bismillaahirrohmaanirroohim. Alhamdulillaahi Robbil ‘aalamin, wal ‘aaqibatu lill Muttaqiin, walaa ‘udwaana illaa 'alazh zhoolimiin, wash sholaatu was salaamu ‘alaa Sayidinaa Muhammadin wa ‘alaa aalihii wa shohbihii ajma’iin, Ammaa ba’du:  
+    ياامام المتقين
+
+    ويا صفوةالعابدين
+
+    _YAA IMAMAL MUTTAQIINA_
+
+    (Artinya: Wahai pemimpin orang-orang yang bertaqwa)
+
+    _WA SHOFWATAL 'AABIDIINA_
+
+    (Artinya: Wahai ahli ibadah yang terbaik)
+
+    وياقوي الاركان
+
+    وياحبيب الرحمان
+
+    _WAYAA QOWIYAL ARKAAN_
+
+    (Artinya: Wahai yang kokoh pilarnya)
+
+    _WAYAA HABIBAR ROHMAAN_
+
+    (Artinya: Wahai kekasih Ar Rohman)
+
+    ويامجلي الكلام القديم
+
+    وياشفاءاسقام السقيم
+
+    _WAYAA MUJLIYAL KALAAMIL QODIMI_
+
+    (Artinya: Wahai yang menyingkap Kalam Al Qodim )
+
+    _YAA SYIFAAA ASQOOMIS SAQIIMI_
+
+    (Artinya: Wahai penyembuh penyakit orang yang sakit)
+
+    (*sesuai dengan Maklumat Guru Agung Hadroti Syeikh Abah Aos Qs, Ahad 23 Romadhon 6 1442 H / 11 Oktober 2020 M)
+
+    بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
+
+    أَلا إِنَّ أَوْلِيَاء اللّهِ لاَ خَوْفٌ عَلَيْهِمْ وَلاَ هُمْ يَحْزَنُونَ, الَّذِينَ آمَنُواْ وَكَانُواْ يَتَّقُونَ,   لَهُمُ الْبُشْرَى فِي الْحَياةِ الدُّنْيَا وَفِي الآخِرَةِ لاَ تَبْدِيلَ لِكَلِمَاتِ اللّهِ ذَلِكَ هُوَ الْفَوْزُ الْعَظِيمُ
+
+    _Bismillaahirrohmaanirroohim._
+
+    _Alaa inna Auliyaa'alloohi laa khoufun alaihim wa laahum yahzanuun. Alladziina aamanuu wakaanuu yattaquun. Lahumul busyroo fil hayaatid dunyaa wa fiil aakhiroh, laa tabdiila likalimaatillaah, dzaalika huwal fauzul azhiim._
+
+
+    _Bismillaahirrohmaanirroohim._
+
+    اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَ. وَالْعَاقِبَةُ لِلْمُتَّقِينَ . وَلَا عُدْوَانَ اِالاَّ عَلىَ الظَّالِمِيْنَ . وَالصَّلاَةُ وَالسَّلاَمُ عَلَى سَيِّدِنَا مُحَمَّدٍ وَعَلَى اَلِهِ وَصَحْبه أَجْمَعِيْن أَمَّا بَعْدُ
+
+    _Alhamdulillaahi Robbil aalamin, wal aaqibatu lill Muttaqiin, walaa udwaana illaa 'alazh zhoolimiin, wash sholaatu was salaamu alaa Sayidinaa Muhammadin wa alaa aalihii wa shohbihii ajma'iin, Ammaa badu:_
 
     Dengan menyebut Nama Alloh Yang Maha Pengasih Maha Penyayang. Puji bagi Alloh pencipta Semesta alam. Sholawat serta salam semoga dilimpahkan kepada junjungan kita Nabi Muhammad SAW berserta keluarganya, sahabatnya serta 'awliya Alloh dan para pengikutnya sampai hari akhir.
 
-    Ini sekelumit manaqib Sulthon Awliya' Syaikh Abdul Qodir Al Jailani, kutipan dari kitab **"Uquudul LaAali Fii Manaaqibil Jayli"** dan kitab **"Tafriihul Khootir Fii Manaaqibisy Syaikhi Abdul Qodir"**, semoga dengan dibacakan manaqib ini, Alloh SWT melimpahkan keberkahannya kepada kita sekalian, terutama kepada Shohibul Hajat (……) dimudahkan rizki yang halal, dijauhkan dari malapetaka dunia dan akhirat, diterima segala niat dan maksud kita, dimudahkan urusan kita yang berhubungan dengan dunia dan akhirat, Amiin Yaa Robbal ‘aalamiin. Adapun diantara manaqib Syaikh Abdul Qodir Al Jailani sebagai berikut: 
-""".trimIndent())
+    Ini sekelumit manaqib Sulthon Awliya' Syaikh Abdul Qodir Al Jailani, kutipan dari kitab "Uquudul LaAali Fii Manaaqibil Jayli" dan kitab "Tafriihul Khootir Fii Manaaqibisy Syaikhi Abdul Qodir", semoga dengan dibacakan manaqib ini, Alloh SWT melimpahkan keberkahannya kepada kita sekalian, terutama kepada Shohibul Hajat  dimudahkan rizki yang halal, dijauhkan dari malapetaka dunia dan akhirat, diterima segala niat dan maksud kita, dimudahkan urusan kita yang berhubungan dengan dunia dan akhirat,  _Amiin Yaa Robbal aalamiin._  Adapun diantara manaqib Syaikh Abdul Qodir Al Jailani sebagai berikut:
+
+    """.trimIndent())
